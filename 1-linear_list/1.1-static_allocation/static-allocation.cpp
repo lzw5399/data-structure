679d38c69a4c68b7cbe9fb4dd9710fc71318427b
zhiwen <lzw5399@gmail.com>
2020-09-19 00:08:18 +0800

    feat: linear list delete

--- 1-linear_list/1.1-static_allocation/static-allocation.cpp
+++ 1-linear_list/1.1-static_allocation/static-allocation.cpp
@@ -5,6 +5,11 @@
  * @Desc: 具体实现方式: 顺序表
  * @Desc: 顺序表的具体[物理结构]实现: 静态分配
  */
+
+#include <iostream>
+
+using namespace std;
+
 #define MAXSIZE 10
 
 // 定义顺序表静态分配的结构体
@@ -21,21 +26,53 @@ void InitList(LinearList &list) {
     list.Length = 0;
 }
 
-// index这里是位序(第几位元素，从1开始)
-bool ListInsert(LinearList &list, int index, int elem) {
+// i这里是位序(第几位元素，从1开始)
+// 方法时间复杂度解析：
+// 最好的是当i = list.Length + 1，正好在最后一位空的，为O(1)
+// 最坏的是当i = 1，在表头，剩余的所有元素都要后移一位，为O(n)
+// 平均复杂度为O(n)
+bool ListInsert(LinearList &list, int i, int elem) {
     // 0. 预检查输入合法性
     if (list.Length >= MAXSIZE) // 存储空间已满，不能插入
         return false;
-    if (index < 1 || index > list.Length + 1) // 判断index范围是否有效
+    if (i < 1 || i > list.Length + 1) // 判断index范围是否有效
         return false;
 
     // 1. 把index往后的元素分别往后移
-    for (int i = list.Length; i >= index; i--) {
-        list.Data[i] = list.Data[i - 1];
+    for (int j = list.Length; j >= i; j--) {
+        list.Data[j] = list.Data[j - 1];
     }
     // 2. 把第index个元素赋值给elem
-    list.Data[index - 1] = elem;
+    list.Data[i - 1] = elem;
     list.Length += 1;
+
+    return true;
+}
+
+// i这里是位序(第几位元素，从1开始)
+// 方法最终会返回删除掉的元素的值，即&elem
+// 算法时间复杂度解析：
+// 最好的是当i = list.Length，正好删除最后一位元素，为O(1)
+// 最坏的是当i = 1，删除表头元素，剩余所有元素都要前移一位，为O(n)
+// 平均复杂度为O(n)
+bool ListDelete(LinearList &list, int i, int &elem) {
+    // 0. 输入参数预检
+    if (i < 1 || i > list.Length)
+        return false;
+
+    // 1. 将即将被删除的数据赋值给elem
+    elem = list.Data[i - 1];
+
+    // 2. 将i后面的元素往前移一位，并将之前的最后一位置为0
+    for (int j = i; j <= list.Length; j++) {
+        if (j != list.Length)
+            list.Data[j - 1] = list.Data[j];
+        else
+            list.Data[j - 1] = 0;
+    }
+
+    // 3. 长度减一
+    list.Length -= 1;
 
     return true;
 }
@@ -44,7 +81,7 @@ int main() {
     LinearList list;
     InitList(list);
 
-    // 填充一些数据
+    // 模拟填充一些数据
     list.Data[0] = 22;
     list.Data[1] = 33;
     list.Data[2] = 44;
@@ -53,8 +90,19 @@ int main() {
     list.Length = 5;
 
     // 调用插入
-    ListInsert(list, 3, 66666);
+    if (ListInsert(list, 3, 66666)) {
+        cout << "插入成功" << endl;
+    } else {
+        cout << "输入不合法，插入失败" << endl;
+    }
 
-    ListInsert(list, 7, 555);
+    // 调用删除
+    int refBack;
+    if (ListDelete(list, 2, refBack)) {
+        cout << "删除成功，被删除掉的元素的值为：" << refBack << endl;
+    } else {
+        cout << "位序i非法，删除失败" << endl;
+    }
+
     return 0;
 }
