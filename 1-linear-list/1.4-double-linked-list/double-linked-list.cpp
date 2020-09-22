2c24c9036e129ecd8a2e1b7dbe3c4c9a55a152ed
zhiwen <lzw5399@gmail.com>
2020-09-22 19:03:15 +0800

    feat: add double linked list

---
+++ 1-linear-list/1.4-double-linked-list/double-linked-list.cpp
@@ -0,0 +1,18 @@
+/**
+ * @Author: lzw5399
+ * @Date: 2020/9/22 19:01
+ * @Desc: 逻辑结构: 线性表
+ * @Desc: 实现方式: 链式存储(链表)
+ * @Desc: 物理结构: 双链表
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "双链表" << endl;
+    
+    return 0;
+}
+
